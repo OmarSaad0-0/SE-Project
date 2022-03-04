a9dc6b574ec2765cb296cca575b7fa5f0a0c32ef
OmarSaad0-0 <89510329+OmarSaad0-0@users.noreply.github.com>
2022-03-04 16:01:19 +0200

    Add files via upload

---
+++ settings.php
@@ -0,0 +1,25 @@
+<html>
+<body>
+<form method="Post" action="applySettings.php">
+<?php
+$conn=mysqli_connect("localhost","root","","clients");
+if($conn->connect_error){
+	die("fatal error");
+}
+
+$query="SELECT * FROM users WHERE id=".$_GET["id"];
+$results=$conn->query($query);
+if(!$results){die("error in the query");}
+while($row=$results->fetch_array(MYSQLI_ASSOC)){
+	echo "<input type=hidden name=id value=".$row["id"]."><br>";
+	echo "<input type=text name=email value=\"".$row["email"]."\"><br>";
+    echo "<input type=text name=password value=\"".$row["password"]."\"><br>";
+    echo "<input type=text name=number value=\"".$row["phone_Number"]."\"><br>";
+
+}
+?>
+<input type=submit>
+
+</form>
+</body>
+</html>
