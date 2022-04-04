7c40afcd70f7fda2552577af8ae6c277cdf2b064
eyadamir19 <96886609+eyadamir19@users.noreply.github.com>
2022-04-05 01:03:22 +0200

    Update index.php

--- mvcframework/app/views/index.php
+++ mvcframework/app/views/index.php
@@ -12,11 +12,11 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
     <!-- custom css file link  -->
-    <link rel="stylesheet" href="http://localhost/MVCFRAMEWORK/public/css/style.css">
+    <link rel="stylesheet" href="style.css">
 
 </head>
 <body>
-  <?php  
+<?php  
 require APPROOT . '/views/includes/navigation.php';
 ?>
 <section class="home" id="home">
@@ -24,7 +24,7 @@ require APPROOT . '/views/includes/navigation.php';
     <div class="content">
         <h3>Clothes Mean <span>Nothing</span> Until Someone Lives in Them</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam libero nostrum veniam facere tempore nisi.</p>
-        <a href="select.html" class="btn">Get Started</a>
+        <a href="functions.int.php" class="btn">Get Started</a>
     </div>
 
 </section>
@@ -65,123 +65,13 @@ require APPROOT . '/views/includes/navigation.php';
 </section>
 
 <!-- features section ends -->
-<!-- products section ends -->
-
-<!-- categories section starts  -->
-
-<section class="categories" id="categories">
-
-    <h1 class="heading"> product <span>categories</span> </h1>
-
-    <div class="box-container">
-
-        <div class="box">
-            <img src="image/TSHIRTS.jpg" alt="">
-            <h3>T-shirts</h3>
-            <p>upto 45% off</p>
-            <a href="#" class="btn">shop now</a>
-        </div>
-
-        <div class="box">
-            <img src="image/SWEAT PANTS.jpg" alt="">
-            <h3>Sweatpants</h3>
-            <p>upto 45% off</p>
-            <a href="#" class="btn">shop now</a>
-        </div>
-
-        <div class="box">
-            <img src="image/POLO TSHIRTS.jpg" alt="">
-            <h3>Polo</h3>
-            <p>upto 45% off</p>
-            <a href="#" class="btn">shop now</a>
-        </div>
-
-        <div class="box">
-            <img src="image/Senior's.jpg" alt="">
-            <h3>Senior's Uniform</h3>
-            <p>upto 45% off</p>
-            <a href="#" class="btn">shop now</a>
-        </div>
-
-    </div>
-
-</section>
-
-<!-- categories section ends -->
-
-<!-- review section starts  -->
-
-<section class="review" id="review">
-
-    <h1 class="heading"> customer's <span>review</span> </h1>
-
-    <div class="swiper review-slider">
-
-        <div class="swiper-wrapper">
-
-            <div class="swiper-slide box">
-                <img src="image/pic-1.png" alt="">
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde sunt fugiat dolore ipsum id est maxime ad tempore quasi tenetur.</p>
-                <h3>john deo</h3>
-                <div class="stars">
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star-half-alt"></i>
-                </div>
-            </div>
-
-            <div class="swiper-slide box">
-                <img src="image/pic-2.png" alt="">
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde sunt fugiat dolore ipsum id est maxime ad tempore quasi tenetur.</p>
-                <h3>john deo</h3>
-                <div class="stars">
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star-half-alt"></i>
-                </div>
-            </div>
-
-            <div class="swiper-slide box">
-                <img src="image/pic-3.png" alt="">
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde sunt fugiat dolore ipsum id est maxime ad tempore quasi tenetur.</p>
-                <h3>john deo</h3>
-                <div class="stars">
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star-half-alt"></i>
-                </div>
-            </div>
-
-            <div class="swiper-slide box">
-                <img src="image/pic-4.png" alt="">
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde sunt fugiat dolore ipsum id est maxime ad tempore quasi tenetur.</p>
-                <h3>john deo</h3>
-                <div class="stars">
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star-half-alt"></i>
-                </div>
-            </div>
-
-        </div>
-
-    </div>
-
-</section>
 
 
 <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
 
 <!-- custom js file link  -->
 <script src="<?php echo URLROOT ?>/public/Js/script.js"></script>
+
 <?php  
 require APPROOT . '/views/includes/footer.php';
 ?>
