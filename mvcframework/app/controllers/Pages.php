f6fcc77de9cd76336a2f5d51492affaf9fa3793e
Adhammhassan <100643227+Adhammhassan@users.noreply.github.com>
2022-04-05 07:12:59 +0200

    Update Pages.php

--- mvcframework/app/controllers/Pages.php
+++ mvcframework/app/controllers/Pages.php
@@ -1,3 +1,5 @@
+
+
 <?php
 class Pages extends Controller {
     public function __construct() {
@@ -14,4 +16,61 @@ class Pages extends Controller {
     public function about() {
         $this->view('about');
     }
+
+    public function Select(){
+        $data = [
+            'title' => 'Home page'
+        ];
+
+        $this->view('users/Select',$data);
+    }
+
+    public function hoodie(){
+        $data = [
+            'title' => 'Home page'
+        ];
+
+        $this->view('users/hoodie',$data);
+    }
+    public function sizing(){
+        $data = [
+            'title' => 'Home page'
+        ];
+
+        $this->view('users/sizing',$data);
+    }
+
+    public function printing(){
+        $data = [
+            'title' => 'Home page'
+        ];
+
+        $this->view('users/printing',$data);
+        
+    }
+
+    public function type(){
+        $data = [
+            'title' => 'Home page'
+        ];
+
+        $this->view('users/type',$data);
+    }
+
+    public function emb(){
+        $data = [
+            'title' => 'Home page'
+        ];
+
+        $this->view('users/emb',$data);
+    }
+
+    public function hood(){
+        $data = [
+            'title' => 'Home page'
+        ];
+
+        $this->view('users/hood',$data);
+    }
+
 }
