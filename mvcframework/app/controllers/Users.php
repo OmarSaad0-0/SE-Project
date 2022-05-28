e9a20c0704a1cfa1cfa8b0f44088a57de793f268
OmarSaad0-0 <89510329+OmarSaad0-0@users.noreply.github.com>
2022-05-28 03:18:31 +0200

    Add files via upload

--- mvcframework/app/controllers/Users.php
+++ mvcframework/app/controllers/Users.php
@@ -222,32 +222,24 @@ public function logout() {
    
    
     header('location:' . URLROOT . '/users/login');
-
 }
 
 
 public function admin_panel(){
-    
-$data=$this->userModel->ViewUsers();
-
-
-    $this->view('users/admin_panel',$data);
-
-}
-
-
-public function admin_panel(){
-    
-    $data = [
-        'userType' => $_SESSION['User_Type']
-        
+    $data=[
+        "title"=>"admin"
     ];
 
     $this->view('users/admin_panel',$data);
 }
-
-
-
+public function ViewOrders(){
+    $data=$this->userModel->ShowOrders();
+    $this->view('users/ViewOrders',$data);
+}
+public function ViewUsers(){
+    $data=$this->userModel->ViewUsers();
+    $this->view('users/ViewUsers',$data);
+}
 
 
 
