cf7023b959642adabc0e76c2b197f66f9294895a
OmarSaad0-0 <89510329+OmarSaad0-0@users.noreply.github.com>
2022-05-30 06:13:11 +0200

    Add files via upload

--- mvcframework/app/views/includes/admin_navigation.php
+++ mvcframework/app/views/includes/admin_navigation.php
@@ -12,9 +12,9 @@ echo "Hello, " .$_SESSION['First_Name'];
         
         <?php endif; ?>
 
-<br><br><a href="http://localhost/mvcframework/users/admin_panel_admins">Admins</a>
-<a href="http://localhost/mvcframework/users/admin_panel_users">Users</a>
-<a href="#">Orders</a>
+<br><br><a href="<?php echo URLROOT ?>/users/admin_panel_admins">Admins</a>
+<a href="<?php echo URLROOT ?>/users/admin_panel_users">Users</a>
+<a href="<?php echo URLROOT ?>/users/Admin_OrdersAction">Orders</a>
 <a href="#">Products</a>
 </nav>
 <br>
