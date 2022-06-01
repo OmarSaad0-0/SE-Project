2c90642926c611e1eed60375cfcb30319f392068
OmarSaad0-0 <89510329+OmarSaad0-0@users.noreply.github.com>
2022-06-01 18:45:11 +0200

    Add files via upload

--- mvcframework/app/views/users/admin_panel_users.php
+++ mvcframework/app/views/users/admin_panel_users.php
@@ -3,15 +3,15 @@
 ?>
 
 
-<?php
- echo "<table border=1 >
+
+ <table class="table table-striped table-hover">
  <th >id</th>
  <th>First Name</th>
  <th>Last Name</th>
  <th>Email</th>
  <th>Number</th>
- ";
-
+ 
+ <?php
 foreach($data as $user){
     if($user->User_Type == 'User'){
     echo "<tr ><td>".$user->Id."</td><td>".$user->First_Name."</td><td>".$user->Last_Name."</td><td>".$user->Brand_Name."</td><td>".$user->User_Number."</td></tr>";
