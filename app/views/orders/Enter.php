15022f0a0c43b87b32043fcdd60d98138c47cefa
OmarSaad0-0 <89510329+OmarSaad0-0@users.noreply.github.com>
2022-06-02 02:46:50 +0200

    Add files via upload

---
+++ app/views/orders/Enter.php
@@ -0,0 +1,40 @@
+
+<form method='POST' action="<?php echo URLROOT; ?>/orders/Enter">
+Fabric<input type='text' name='Fabric'>
+Base<input type='text' name='Base'>
+Colour<input type='text' name='Colour'>
+Quantity<input type='text' name='Quantity'>
+Info<input type='text' name='Info'>
+Time<input type='text' name='Time'>
+<input type='submit'>
+
+<?php
+echo "<select>";
+foreach ($data['Pr'] as $a)
+{
+    foreach($a as $key => $value)
+    {
+        echo "<option selected='selected' value= '$value'> $value</option>";
+    }
+    echo "<br>";
+}
+   echo "</select>" ;
+?>
+
+
+</form>
+
+
+
+
+<?php
+    $_SESSION['Base']=$_POST['Base'];
+    $_SESSION['Fabric']=$_POST['Fabric'];
+    $_SESSION['Colour']=$_POST['Colour'];
+    $_SESSION['Printing']=$_POST['Printing'];
+    $_SESSION['Printing']=$_POST['Quantity'];
+    $_SESSION['Quantity']=$_POST['Quantity'];
+    $_SESSION['Info']=$_POST['Info'];
+    $_SESSION['Time']=$_POST['Time'];
+    $_SESSION['Pr']=$data['Pr'];
+
