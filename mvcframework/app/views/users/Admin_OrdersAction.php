f9ac4dcb4ac15a2523b1ec624b7c7632aab7d31e
nouka01 <96498521+nouka01@users.noreply.github.com>
2022-06-03 23:16:55 +0200

    Update Admin_OrdersAction.php

--- mvcframework/app/views/users/Admin_OrdersAction.php
+++ mvcframework/app/views/users/Admin_OrdersAction.php
@@ -17,7 +17,7 @@
 <th>Order status</th>
 <?php
 foreach($data as $order){
-   echo "<tr ><td>".$order->Id."</td><td>".$order->Ordered_By."</td><td>".$order->base_product."</td><td>".$order->Fabric."</td><td>".$order->Colour."</td><td>".$order->printing_Type."</td><td>".$order->Quantity."</td><td>".$order->additionalInfo."</td><td>".$order->deliveryTime."</td>
+   echo "<tr ><td>".$order->Id."</td><td>".$order->Ordered_By."</td><td>".$order->base_product."</td><td>".$order->Fabric."</td><td>".$order->Colour."</td><td>".$order->Printing."</td><td>".$order->Quantity."</td><td>".$order->additionalInfo."</td><td>".$order->deliveryTime."</td>
    <td>".$order->Order_date."</td><td>".$order->Order_status."</td><td><form action ='http://localhost/mvcframework/users/Admin_OrdersAction?action=decline&id=$order->Id' method = 'POST'>
       <div class='opacity-75'><input type = 'submit' name = 'decline-button' value ='Decline'  class='btn btn-dark' ></input></div>
       </form><form action ='http://localhost/mvcframework/users/Admin_OrdersAction?action=accept&id=$order->Id' method = 'POST'>
